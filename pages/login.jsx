9e0224ac34d1c54c6932db87df096816427360e3
mteguhpro <mteguhpro@gmail.com>
2023-04-16 10:21:32 +0700

    menambah peringatan web demo

--- pages/login.jsx
+++ pages/login.jsx
@@ -8,28 +8,28 @@ import { toast } from 'react-toastify';
 export default function Login() {
     const router = useRouter()
 
-    useEffect(function() {
-        if(window.localStorage.getItem("jwtToken")){
+    useEffect(function () {
+        if (window.localStorage.getItem("jwtToken")) {
             router.push('/')
         }
     }, [])
     //POST
-    async function sendReq(url, data){
+    async function sendReq(url, data) {
         const post = await instanceAxios.post(url, data?.arg).then(res => res.data)
-            .catch(function(err){
-                toast.error('Error '+ err.response.status + ' | ' +err.response.data.message)
+            .catch(function (err) {
+                toast.error('Error ' + err.response.status + ' | ' + err.response.data.message)
             })
-        if(post?.token){
+        if (post?.token) {
             window.localStorage.setItem("jwtToken", post.token)
-            toast.success('Selamat datang, '+post?.username)
+            toast.success('Selamat datang, ' + post?.username)
             router.push('/')
-        }else{
+        } else {
             toast.error('Gagal Login. Token tidak ditemukan.')
         }
     }
     const address = 'auth/login';
     const { trigger, isMutating } = useSWRMutation(address, sendReq)
-    
+
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     const onSubmit = data => trigger(data)
@@ -56,8 +56,23 @@ export default function Login() {
                         {isMutating ? 'Loading...' : <button className="btn btn-sm my-3">Login</button>}
                     </div>
 
-                    <div className="text-gray-500">
-                        Demo username: <code>atuny0</code> password: <code>9uQFF1Lh</code>
+                    <div className="alert shadow-lg">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            <span>
+                                <b className="text-bold">Tips: </b>Anda bisa menggunakan akun berikut untuk kebutuhan demo,
+                                <br/>
+                                username: <code>atuny0</code> password: <code>9uQFF1Lh</code>
+                            </span>
+                        </div>
+                    </div>
+                    <div className="alert alert-warning shadow-lg mt-2">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+                            <span>
+                                <b className="text-bold">Peringatan: </b>Ini adalah web demo. Insert data, Update data dan Delete data Tidak akan mengubah data pada server!
+                            </span>
+                        </div>
                     </div>
 
                 </form>
